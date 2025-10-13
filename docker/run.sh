c4f904bb39a3768bba139c0d243048aba06f85fc
Gwidi <gwidon.szczepankiewicz@student.put.poznan.pl>
2025-10-13 12:35:18 +0200

    Modify run file

--- docker/run.sh
+++ docker/run.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
 docker run -it \
+    --name znimpra_container \
     -v /home/gwidon/Documents/ZNIMPRA:/home/ZNIMPRA \
-    --env ROS_DOMAIN_ID=11
+    --env ROS_DOMAIN_ID=11 \
+    --runtime=nvidia \
+    --env="DISPLAY" \
+    --env="NVIDIA_VISIBLE_DEVICES=all" \
+    --env="NVIDIA_DRIVER_CAPABILITIES=all" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:ro" \
+    --privileged \
+    znimpra_ros bash
+
