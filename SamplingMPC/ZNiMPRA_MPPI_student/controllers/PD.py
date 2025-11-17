2b803c77b2861462e278eb41efddc9bf6206a5ce
Gwidi <gwidon.szczepankiewicz@student.put.poznan.pl>
2025-11-17 15:37:07 +0100

    fix: PD controller

--- SamplingMPC/ZNiMPRA_MPPI_student/controllers/PD.py
+++ SamplingMPC/ZNiMPRA_MPPI_student/controllers/PD.py
@@ -2,12 +2,14 @@ from controllers.interface import Controller
 import numpy as np
 
 class PDController(Controller):
-    def compute_control(self, initial_state, kp=10.0, kd=2.0):
-        observation = initial_state
-        desired_state = np.array([0.0, 0.0])
+    def compute_control(self, observation, kp=10.0, kd=1.0):
+        # Pendulum-v1 observation format: [cos(theta), sin(theta), theta_dot]
+        cos_theta, sin_theta, theta_dot = observation
+        theta = np.arctan2(sin_theta, cos_theta)
+        desired_theta, desired_theta_dot = 0.0, 0.0 # Upright position
         # Desired position is upright (theta = 0)
-        error = desired_state[0] - observation[0]
-        error_dot = desired_state[1] - observation[1]
+        error = desired_theta - theta
+        error_dot = desired_theta_dot - theta_dot
         # PD control law
         u = kp * error + kd * error_dot
         # Clip action to be within action space
--- SamplingMPC/ZNiMPRA_MPPI_student/experiment.py
+++ SamplingMPC/ZNiMPRA_MPPI_student/experiment.py
@@ -11,11 +11,12 @@ controller = PDController(env)
 env.reset()
 initial_state = np.array([0.1, 0.5])
 env.unwrapped.state = initial_state
+observation = env.unwrapped._get_obs()
 
 episode_reward = 0.
 for i in range(n_steps):
-    action = controller.compute_control(initial_state)
-    next_observation, reward, terminated, truncated, info = env.step(action)
+    action = controller.compute_control(observation=observation)
+    observation, reward, terminated, truncated, info = env.step(action)
     episode_reward += reward
     env.render()
 print(f"Episode reward: {episode_reward}")
