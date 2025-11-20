2429ff3c32cd2724eed293e4dd7ac9ee30f3448c
Gwidi <gwidon.szczepankiewicz@student.put.poznan.pl>
2025-11-20 22:00:01 +0100

    fix: SwingUpController

--- SamplingMPC/ZNiMPRA_MPPI_student/controllers/SwingUp.py
+++ SamplingMPC/ZNiMPRA_MPPI_student/controllers/SwingUp.py
@@ -2,10 +2,16 @@ from controllers.interface import Controller
 import numpy as np
 
 class SwingUpController(Controller):
-    def compute_control(self):
-        E = 1/2 * self.env.unwrapped.J * self.env.state[1]**2 + self.env.unwrapped.m * self.env.unwrapped.g * self.env.unwrapped.l * (np.cos(self.env.state[0]) - 1)
+    def __init__(self, env):
+        super().__init__(env)
+        self.J = self.env.unwrapped.m * self.env.unwrapped.l ** 2
+    def compute_control(self, observation):
+        # Pendulum-v1 observation format: [cos(theta), sin(theta), theta_dot]
+        cos_theta, sin_theta, theta_dot = observation
+        theta = np.arctan2(sin_theta, cos_theta)
+        E = 0.5 * self.J * theta_dot**2 + self.env.unwrapped.m * self.env.unwrapped.g * self.env.unwrapped.l * (np.cos(theta) - 1)
         E_desired = 0.0
         u_max = self.env.action_space.high[0]
-        u = u_max * np.sign(E - E_desired)* self.env.state[1] * np.cos(self.env.state[0])
+        u = u_max * self.env.unwrapped.g * np.sign(E - E_desired)* theta_dot * np.cos(theta)
         u = np.clip(u, self.env.action_space.low[0], self.env.action_space.high[0])
         return np.array([u])
