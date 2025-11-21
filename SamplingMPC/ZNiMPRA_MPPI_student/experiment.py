50e148420de769d2cdd0fd405791f5521ece777a
Gwidi <gwidon.szczepankiewicz@student.put.poznan.pl>
2025-11-21 22:16:03 +0100

    Do switching between controllers

--- SamplingMPC/ZNiMPRA_MPPI_student/experiment.py
+++ SamplingMPC/ZNiMPRA_MPPI_student/experiment.py
@@ -3,20 +3,30 @@ import numpy as np
 
 from controllers.random import RandomController
 from controllers.PD import PDController
+from controllers.SwingUp import SwingUpController
 
 n_steps = 200
 env = gym.make("Pendulum-v1", render_mode="human", g=9.81)
-controller = PDController(env)
+controller1 = SwingUpController(env)
+controller2 = PDController(env)
 
 env.reset()
-initial_state = np.array([0.1, 0.5])
+initial_state = np.array([3.1, 0.0])
 env.unwrapped.state = initial_state
 observation = env.unwrapped._get_obs()
+cos_theta, sin_theta, theta_dot = observation
+theta = np.arctan2(sin_theta, cos_theta)
 
 episode_reward = 0.
 for i in range(n_steps):
-    action = controller.compute_control(observation=observation)
+    if abs(theta) < np.pi/4:
+        action = controller2.compute_control(observation=observation)
+        print(observation)
+    else:
+        action = controller1.compute_control(observation=observation)
     observation, reward, terminated, truncated, info = env.step(action)
+    cos_theta, sin_theta, theta_dot = observation
+    theta = np.arctan2(sin_theta, cos_theta)
     episode_reward += reward
     env.render()
 print(f"Episode reward: {episode_reward}")
