c5b05a54c8b6c2f4a7cf6760a179f694a68ff7df
Gwidi <gwidon.szczepankiewicz@student.put.poznan.pl>
2025-11-21 22:14:52 +0100

    Modify desired Energy in SwingUp Controller

--- SamplingMPC/ZNiMPRA_MPPI_student/controllers/SwingUp.py
+++ SamplingMPC/ZNiMPRA_MPPI_student/controllers/SwingUp.py
@@ -10,8 +10,8 @@ class SwingUpController(Controller):
         cos_theta, sin_theta, theta_dot = observation
         theta = np.arctan2(sin_theta, cos_theta)
         E = 0.5 * self.J * theta_dot**2 + self.env.unwrapped.m * self.env.unwrapped.g * self.env.unwrapped.l * (np.cos(theta) - 1)
-        E_desired = 0.0
+        E_desired = self.env.unwrapped.m * self.env.unwrapped.g * self.env.unwrapped.l
         u_max = self.env.action_space.high[0]
-        u = u_max * self.env.unwrapped.g * np.sign(E - E_desired)* theta_dot * np.cos(theta)
+        u = u_max * self.env.unwrapped.g * np.sign((E - E_desired) * theta_dot * np.cos(theta))
         u = np.clip(u, self.env.action_space.low[0], self.env.action_space.high[0])
         return np.array([u])
